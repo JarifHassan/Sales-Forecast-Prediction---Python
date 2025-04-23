5081e0133d6dbeb40fdef472eb879dab92636547
JarifHassan <hassanjarif@gmail.com>
2025-04-23 13:56:29 +1200

    Update Sales Forecast Prediction – Python.py

--- Sales Forecast Prediction – Python.py
+++ Sales Forecast Prediction – Python.py
@@ -37,3 +37,13 @@ def create_lagged_features(data, lag=1):
 lag = 5
 sales_with_lags = create_lagged_features(data[['Order Date', 'Sales']], lag)
 sales_with_lags = sales_with_lags.dropna()
+
+#5. Preparing the Data for Training
+
+X = sales_with_lags.drop(columns=['Order Date', 'Sales'])
+y = sales_with_lags['Sales']
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, shuffle=False)
+
+#Training the XGBoost Model
+
+model
