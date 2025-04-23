1d9bbdabac8e90c6f8128c52ce9d77d2af307e02
JarifHassan <hassanjarif@gmail.com>
2025-04-23 13:45:27 +1200

    Update Sales Forecast Prediction – Python.py

--- Sales Forecast Prediction – Python.py
+++ Sales Forecast Prediction – Python.py
@@ -9,3 +9,12 @@ from sklearn.metrics import mean_squared_error
 file_path = 'train.csv'
 data = pd.read_csv(file_path)
 data.head()
+
+# 3. Data Preprocessing and Visualization
+
+data['Order Date'] = pd.to_datetime(data['Order Date'], format='%d/%m/%Y')
+
+sales_by_date = data.groupby('Order Date')['Sales'].sum().reset_index()
+
+plt.figure(figsize=(12,6))
+plt.plot(sales_by_date['Order Date'], sales_by_date['Sales'], label= 'Sales', color='red')
